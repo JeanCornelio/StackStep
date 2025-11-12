3a3d7e970afa1ca470b973f6f566222c7c78d33f
JeanCornelio <jean_cornelio17@hotmail.com>
2025-11-12 12:26:41 -0500

    Refactor CustomDatePicker and AddGoal components for improved functionality and code organization

--- src/pages/dashboard/GoalsPage.tsx
+++ src/pages/dashboard/GoalsPage.tsx
@@ -33,7 +33,7 @@ export const GoalsPage = () => {
         </div>
       </div>
 
-      <div className="grid grid-cols-1 sm:grid-cols-2  2xl:grid-cols-3 gap-4  w-full">
+      <div className="grid grid-cols-1 md:grid-cols-2  lg:grid-cols-3 2xl:grid-cols-4 gap-4  w-full">
         <GoalCardComponent />
         <GoalCardComponent />
         <GoalCardComponent />
